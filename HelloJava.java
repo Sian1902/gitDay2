763f84a63b1c64137fc83f45f30559cce27fdbee
Ahmed Haitham <96428026+Sian1902@users.noreply.github.com>
2025-12-28 11:39:45 +0200

    added a new class HelloITI with a mehtod printHelloGit

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,10 @@
-public  class HelloWorld{
+public  class Hellojava{
 	public void  printHellow(){
 		System.out.prrintln("Hello world");
+	}
+}
+class HelloITI{
+	public void  printHelloGit(){
+		System.out.prrintln("Hello Git"); 
 	}
 }
